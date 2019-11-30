db8f44ac7c0dfa7aec18b2005d1b1335871c1900
Ervens Achille <55925419+ErvensAchille@users.noreply.github.com>
2019-11-30 17:14:24 -0500

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:demo@cluster0-ngeyt.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
+// mongodb+srv://demo:<password>@cluster0-ngeyt.mongodb.net/test?retryWrites=true&w=majority
